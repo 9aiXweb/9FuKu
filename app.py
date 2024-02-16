79e01d9ede624ba463a0e2d84a79d7344d776490
KawanoRyoya <54755448+RyoyaKawano@users.noreply.github.com>
2024-02-16 07:33:57 +0000

    t

--- app.py
+++ app.py
@@ -10,20 +10,20 @@ def main_stock():
     if request.method == 'POST':
         if(request.form.get('input')  == None):
             return render_template('index.html')
-    
-        inputs = str(request.form['input'])
-        pipe = pipeline("text-generation", model="TinyLlama/TinyLlama-1.1B-Chat-v1.0", torch_dtype=torch.bfloat16)#, device_map="auto")
+        outputs = str(request.form['input'])
+        # inputs = str(request.form['input'])
+        # pipe = pipeline("text-generation", model="TinyLlama/TinyLlama-1.1B-Chat-v1.0", torch_dtype=torch.bfloat16)#, device_map="auto")
 
-        # We use the tokenizer's chat template to format each message - see https://huggingface.co/docs/transformers/main/en/chat_templating
-        messages = [
-            {
-                "role": "system",
-                "content": "You are a friendly chatbot who always responds in the style of a pirate",
-            },
-            {"role": "user", "content": inputs},
-        ]
-        prompt = pipe.tokenizer.apply_chat_template(messages, tokenize=False, add_generation_prompt=True)
-        outputs = pipe(prompt, max_new_tokens=256, do_sample=True, temperature=0.7, top_k=50, top_p=0.95)
+        # # We use the tokenizer's chat template to format each message - see https://huggingface.co/docs/transformers/main/en/chat_templating
+        # messages = [
+        #     {
+        #         "role": "system",
+        #         "content": "You are a friendly chatbot who always responds in the style of a pirate",
+        #     },
+        #     {"role": "user", "content": inputs},
+        # ]
+        # prompt = pipe.tokenizer.apply_chat_template(messages, tokenize=False, add_generation_prompt=True)
+        # outputs = pipe(prompt, max_new_tokens=256, do_sample=True, temperature=0.7, top_k=50, top_p=0.95)
 
         html = render_template('index.html', output=outputs)
 
